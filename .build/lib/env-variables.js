11e9b389860d1051ff903ab3dbbe01a62ef5f1c9
Graham King <gking2224@gmail.com>
2019-02-11 07:56:53 +0000

    try vscode debug

--- .build/lib/env-variables.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    MessagesTable: process.env.messages_table,
-    ConnectionsTable: process.env.connections_table
-};
-//# sourceMappingURL=env-variables.js.map
